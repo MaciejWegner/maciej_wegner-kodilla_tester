cfff364407fd5b57ac1bec50fc07d4f4f95e0804
Maciej Wegner <maciekwegner@gmail.com>
2021-09-29 23:15:03 +0200

    Zadanie Hello World!

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {                      // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello Word");
+        System.out.println("Maciej");
+        System.out.println("43");
+        System.out.println("W");
+    }
+
+}
