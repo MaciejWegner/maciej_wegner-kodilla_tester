3d2846c16080d34b1dc640f390e97b52902c3f8e
Maciej Wegner <maciekwegner@gmail.com>
2021-10-02 11:34:04 +0200

    Modul 8

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {public static void main(String[] args) {
+    String myName = "Maciej";
+    int myAge = 43;
+    double height = 1.74;
+    char carClass = 'A';
+    boolean isGoodProgrammer = false;
+    String myDog = "Iskierka";
+    System.out.println(myName);
+    System.out.println(myAge);
+    System.out.println(height);
+    System.out.println(carClass);
+    System.out.println(myDog);
+    System.out.println(isGoodProgrammer);
+
+}
+}
