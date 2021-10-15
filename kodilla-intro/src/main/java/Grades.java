8262f8da621866bc149b0ec6576d1fc49813cbc5
Maciej Wegner <maciekwegner@gmail.com>
2021-10-15 21:35:22 +0200

    moduł 2.4 rev1

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -2,14 +2,11 @@ public class Grades {
 
     private int[] grades;
     private int size;
-    private int averageGrade;
-
 
     public Grades() {
         this.grades = new int[10];
         this.size = 0;
-        this.averageGrade = 0;
-    }
+                    }
 
     public void add(int value) { // dodającą przekazaną w argumencie ocenę do tablicy
         if (this.size == 10) {
@@ -17,10 +14,11 @@ public class Grades {
         }
         this.grades[this.size] = value;
         this.size++;
+
     }
 
 
-    public void srednia(int[] grades) {
+    public int srednia(int[] grades) {
 
         int suma = 0;
         int iloscOcen = grades.length;
@@ -30,18 +28,21 @@ public class Grades {
 
         // dzielimy zsumowaną tablicę przez ilość ocen w tablicy
         int averageGrade = suma / iloscOcen;
+                    return averageGrade;
+    }
 
-        System.out.println(averageGrade);}
-
-    public void ostaniaWartosc(int[] grades) {
+    public int ostaniaWartosc(int[] grades) {
         int iloscOcen = grades.length;
                 grades = new int [iloscOcen];
         for(int i=0; i<grades.length; i++)
             grades[i] = i+1;
         int licznik=0;
-        while(licznik<grades.length){
-            System.out.print(grades[licznik]+" ");
-        licznik++;}
+        while(licznik<grades.length)
+        licznik++;
+        int wys=grades[licznik];
+
+        {
+            return wys; }
         }
     }
     //nie wiem jak zrobić "zwracanie ostatnio dodanej oceny" - chciałbym to przedyskutować
