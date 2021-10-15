926122aad054c535fafb1032f167842aa132b56a
Maciej Wegner <maciekwegner@gmail.com>
2021-10-15 10:17:20 +0200

    moduł 2.3 rev1

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -2,19 +2,19 @@ public class FirstClass {
     public static void main(String[] args) {
 
         Notebook notebook = new Notebook(600, 1000, 2016);
-        System.out.println(notebook.weight + "g" + " " + notebook.price + "zl" + " " + notebook.year);
+        System.out.println(notebook.weight + "g" + " " + notebook.price + "zl" + " " + notebook.year + " " + "rok Produkcji");
         notebook.checkPrice();
         notebook.checkWeight();
         notebook.checkYear();
 
         Notebook heavyNotebook = new Notebook(2000, 1500, 2018);
-        System.out.println(heavyNotebook.weight + "g" + " " + heavyNotebook.price + "zl" + " " + notebook.year);
+        System.out.println(heavyNotebook.weight + "g" + " " + heavyNotebook.price + "zl" + " " + notebook.year + " " + "rok Produkcji");
         heavyNotebook.checkPrice();
         heavyNotebook.checkWeight();
         heavyNotebook.checkYear();
 
         Notebook oldNotebook = new Notebook(1600, 500, 2021);
-        System.out.println(oldNotebook.weight + "g" + " " + oldNotebook.price + "zl" + " " +notebook.year);
+        System.out.println(oldNotebook.weight + "g" + " " + oldNotebook.price + "zl" + " " +notebook.year + " " + "rok Produkcji");
         oldNotebook.checkPrice();
         oldNotebook.checkWeight();
         oldNotebook.checkYear();
