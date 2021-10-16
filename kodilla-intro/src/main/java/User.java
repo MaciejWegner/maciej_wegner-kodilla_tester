58d4cb7000a651a0fff34f25f94acf2ecbf0e0fc
Maciej Wegner <maciekwegner@gmail.com>
2021-10-16 12:01:42 +0200

    moduł 2.5 rev1

---
+++ kodilla-intro/src/main/java/User.java
@@ -0,0 +1,14 @@
+public class User {
+    String name;
+    int age;
+    public User( String name, int age) {
+        this.name= name;
+        this.age=age;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getName() {
+        return name;
+    }
+}
