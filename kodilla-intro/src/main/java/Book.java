e20a5eec93bf1ae7b7d937fd7d6f8d2794828f8b
Maciej Wegner <maciekwegner@gmail.com>
2021-10-18 21:35:14 +0200

    moduł 2.7

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    static String author;
+    static String title;
+
+    public  Book (String author, String title)
+    {
+
+        this.author = author;
+        this.title = title;
+    }
+}
+//nie mogę wstawić private String author
+//oraz
+//
+//private String title
---
+++ kodilla-intro/src/main/java/BookApplication.java
@@ -0,0 +1,10 @@
+public class BookApplication {
+    public static void main(String[] args) {
+        Book book1 = new Book("Isaac Asimov", "The Galaxy"); {
+System.out.println(book1.author +book1.title);
+        }
+    }
+}
+
+
+         //nie udało mi się wstawić ook book = Book.of("Isaac Asimov", "The Galaxy");
