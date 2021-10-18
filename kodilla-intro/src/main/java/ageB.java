7ffed960f8d168bb0a147285bc10b98ebaec26b1
Maciej Wegner <maciekwegner@gmail.com>
2021-10-18 11:52:58 +0200

    moduł 2.6

---
+++ kodilla-intro/src/main/java/ageB.java
@@ -0,0 +1,24 @@
+public class ageB {
+    public static void main(String[]args){
+        ageA person1=new ageA("Adam", 30, 178);
+               ageA.checkAge();
+
+    }
+}
+
+
+//public class Application {
+//  public static void main(String[] args) {
+//    String name = "Adam";
+//  double age = 40.5;
+//double height = 178;
+
+//if (name != null) {
+//  if (age > 30 && height > 160) {
+//    System.out.println("User is older than 30 and higher than 160cm");
+//} else {
+//    System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+//}
+//}
+//}
+//}
