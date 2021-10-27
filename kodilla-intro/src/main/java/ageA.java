dea5284356265d67c22bb98bb915dbdf35fbc39e
Maciej Wegner <maciekwegner@gmail.com>
2021-10-27 09:38:43 +0200

    Moduł 2.7 rev2

--- kodilla-intro/src/main/java/ageA.java
+++ kodilla-intro/src/main/java/ageA.java
@@ -1,34 +1,35 @@
 public class ageA {
-   String name;
-   double age;
-   double height;
+    String name;
+    double age;
+    double height;
 
-  public ageA(String name, double age, double height) {
-     this.name = name;
-    this.age = age;
-    this.height = height;
+    public ageA(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
     }
 
     public void checkAge() {
-      if (name !=null) {
-          if (age > 30 && height > 160) {
-              System.out.println("User is older than 30 and higher than 160cm");
-          } else {
-              System.out.println("User is 30 (or younger) or 160cm (or shorter)");
-          }
-      }}}
+        if (name != null) {
+            if (age > 30) {
+                System.out.println("User is older than 30");
+            } else {
+                System.out.println("User is 30 or younger");
+            }
+        }
+    }
 
-//public class Application {
-  //public static void main(String[] args) {
-    //String name = "Adam";
-  //double age = 40.5;
-//double height = 178;
-//if (name != null) {
- //if (age > 30 && height > 160) {
- //System.out.println("User is older than 30 and higher than 160cm");
-//} else {
-  // System.out.println("User is 30 (or younger) or 160cm (or shorter)");
-//}
-//}
-//}
-//}
+    public void checkHeight() {
+        if (name != null) {
+            if (height > 30) {
+                System.out.println("User is higher than 160 cm");
+            } else {
+                System.out.println("User is shorter than 160 cm");
+            }
+        }
+    }
+
+    public void checkName() {
+        System.out.println(name);
+    }
+}
--- kodilla-intro/src/main/java/ageB.java
+++ kodilla-intro/src/main/java/ageB.java
@@ -3,22 +3,7 @@ public class ageB {
         ageA person1 = new ageA("Adam", 30, 178);
         System.out.println(person1.name + " " + person1.age + " " + person1.height);
         person1.checkAge();
+        person1.checkHeight();
+        person1.checkName();
     }
 }
-
-
-//public class Application {
-//  public static void main(String[] args) {
-//    String name = "Adam";
-//  double age = 40.5;
-//double height = 178;
-
-//if (name != null) {
-//  if (age > 30 && height > 160) {
-//    System.out.println("User is older than 30 and higher than 160cm");
-//} else {
-//    System.out.println("User is 30 (or younger) or 160cm (or shorter)");
-//}
-//}
-//}
-//}
