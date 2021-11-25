cc716c4dc24d217ec332321816f2e042570e9736
Maciej Wegner <maciekwegner@gmail.com>
2021-11-25 21:20:58 +0100

    7.4

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Order.java
@@ -0,0 +1,33 @@
+package com.kodilla.execution_model.homework;
+
+import java.time.LocalDate;
+
+public class Order {
+    private final double orderValue;
+    private final LocalDate orderDate;
+    private final String login;
+
+    public Order(double orderValue, LocalDate orderDate, String login) {
+        this.orderValue = orderValue;
+        this.orderDate = orderDate;
+        this.login = login;
+    }
+
+    public double getOrderValue() {
+        return orderValue;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderValue=" + orderValue +
+                ", orderDate=" + orderDate +
+                ", login='" + login + '\'' +
+                '}';
+    }
+}
